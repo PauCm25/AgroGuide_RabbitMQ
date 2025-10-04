a2c3c16bdb8488ee304812f6b5c101aed86a93b8
daniela1415 <dandreacastro@ucundinamarca.edu.co>
2025-10-03 19:36:55 -0500

    Actualización conexion.py
    Actualización para que sirva en general

--- conexion.py
+++ conexion.py
@@ -2,14 +2,7 @@ import pika
 
 def conectar_rabbitmq():
     """Establece conexión con RabbitMQ en localhost."""
-
-    """Credenciales del RabbitMQ"""
-    credentials = pika.PlainCredentials('paula', '123456')
-
-    conexion = pika.BlockingConnection(
-        pika.ConnectionParameters(host="localhost",credentials=credentials)
-        )
-    
+    conexion = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     canal = conexion.channel()
     return canal
 
